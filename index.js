208d175c9eb1e47570cac7343266429d75819d0e
28111600 <28111600@qq.com>
2016-08-11 18:55:59 +0800

    database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var P2PSpider = require('./lib');
-var mysql = require('mysql');
+var write2Database = require('./lib/write2Database');
 
 var config = require('./config');
 
@@ -21,18 +21,7 @@ var count = 0;
 var lengthQueue = config.lengthQueue || 10;
 var arrayQueue = [];
 
-var pool = mysql.createPool({
-    host: config.db_host,
-    user: config.db_user,
-    password: config.db_password,
-    database: config.db_database,
-    port: config.db_port
-});
 
-var writetodatabase = function (arrayMetadata, pool) {
-
-
-}
 
 var getFullName = function (path) {
     var chrPathSplit = "/";
@@ -56,8 +45,6 @@ p2p.on('metadata', function (metadata) {
 
     arrayQueue.push(data);
 
-
-
     if (metadata.info.files) {
 
         for (var i = 0; i < metadata.info.files.length; i++) {
@@ -69,7 +56,7 @@ p2p.on('metadata', function (metadata) {
                     filename: getFullName(itemFileinfo.path),
                     length: itemFileinfo.length
 
-                })
+                });
             }
         }
     } else {
@@ -99,86 +86,12 @@ p2p.on('metadata', function (metadata) {
 
         var subArrayQueue = [].concat(arrayQueue);
         arrayQueue = [];
-
-        pool.getConnection(function (err, conn) {
-
-            conn.beginTransaction(function (err) {
-                if (err) { throw err; }
-                var subCount = 0;
-
-                var arrayPost = [];
-                var arrayQuery = [];
+        write2Database(arrayQueue, config);
 
 
-
-                for (var i = 0; i < subArrayQueue.length; i++) {
-
-                    var data = subArrayQueue[i];
-
-
-                    for (var j = 0; j < data.files.length; j++) {
-
-                        var itemFileinfo = data.files[j];
-                        var post = [data.hash, itemFileinfo.filename, itemFileinfo.length, data.hash];
-                        var query = 'insert into p2pspider_files (hash,filename,length) select * from ( select ?,?,? ) as temp where not exists (select * from p2pspider where hash=?);';
-
-                        conn.query(query, post, function (err, result) {
-
-                        })
-                    };
-
-
-                    var post = [data.hash, data.name, data.length, data.magnet, data.fetchedAt, data.hash];
-                    var query = 'insert into p2pspider (hash,name,length,magnet,fetched) select * from ( select ?,?,?,?,? ) as temp where not exists (select * from p2pspider where hash=?);';
-
-                    conn.query(query, post, function (err, result) {
-
-                        if (!err) {
-
-                            if (result.affectedRows) {
-                                subCount += result.affectedRows;
-
-                            }
-
-
-                        }
-                    });
-                }
-
-                conn.commit(function (err) {
-
-                    if (err) {
-                        conn.rollback(function () {
-                            throw err;
-                        });
-                    }
-                    count += subCount;
-                    console.log(subCount + ' / ' + count);
-                    console.log('success!');
-
-                    conn.release();
-
-                });
-
-
-
-            });
-        })
     }
 });
 
-/*
-{ info: 
-   { files: [ [Object], [Object] ],
-     name: 'VNDS-3070',
-     'piece length': 2097152,
-     pieces: <Buffer 69 25 00 32 11 a9 83 3e 1f 51 5e 86 b9 08 1b e0 cd da 5d 5c 05 a3 3b 2f 08 4b 1a b4 3d 55 e1 5c 14 1e e2 5e c3 ee 5d 2e 4b a0 05 e0 fe be e2 c8 f6 41 ... > },
-  address: '125.205.199.222',
-  port: 22695,
-  infohash: '598373d041d9378cfa28e3cbda4305767477ff4d',
-  magnet: 'magnet:?xt=urn:btih:598373d041d9378cfa28e3cbda4305767477ff4d' }
-
- */
 if (config.p2p_port.length) {
     for (var i = 0; i < config.p2p_port.length; i++) {
         p2p.listen(config.p2p_port[i], config.p2p_listen);
